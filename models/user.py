1c255532c5b59d8c208bb4caf42372d6116aff8b
nancy nyokabi <nancynyokabi1995@gmail.com>
2022-02-06 23:05:13 +0300

    Added a Comment class to the models, user.py

--- models/user.py
+++ models/user.py
@@ -64,3 +64,16 @@ class Pitch(db.Model):
 
     def __repr__(self):
         return f'Pitch {self.post}'
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    pitch_id = db.Column(db.Integer, db.ForeignKey(
+        'pitches.id'), nullable=False)
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
