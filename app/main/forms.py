2efb3ab81595b7183f108f9f27edd12bd550c01a
nancy nyokabi <nancynyokabi1995@gmail.com>
2022-02-09 10:26:11 +0300

    Changed the background color

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,7 +7,7 @@ class PitchForm(FlaskForm):
     title = StringField('pitch_title')
     text = TextAreaField('pitch_text')
     category = SelectField('pitch_type', choices=[(
-        'technology', 'Tech-Pitch'), ('travels', 'Travels-Pitch'), ('sports', 'Sports-Pitch')])
+        'technology', 'Product-Pitch'), ('travels', 'Interview-Pitch'), ('sports', 'Promotion-Pitch')])
     submit = SubmitField('submit')
 
 
